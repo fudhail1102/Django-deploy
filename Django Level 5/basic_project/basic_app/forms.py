dd287c62862ee6d424cfe67871685dd07a9cc07f
Fudhail M <fudhail@Fudhails-MacBook-Air.local>
2024-03-23 19:25:32 +0530

    first commit

---
+++ Django Level 5/basic_project/basic_app/forms.py
@@ -0,0 +1,16 @@
+from basic_app.models import UserProfile
+from django.contrib.auth.models import User
+from django import forms
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        model = User
+        fields = ["username","password","email"]
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ['profile_pic']
+
